b13cfbc7fda693e276d4d15f8b2af14f7f55f8ea
yooe <yooe05@gmail.com>
2023-12-21 11:16:44 +0300

    add enter data validation

--- ArticleGameServer/Controllers/useraccountcontroller.js
+++ ArticleGameServer/Controllers/useraccountcontroller.js
@@ -1,3 +1,8 @@
+const { servTrySaveCreds,
+    servTrySavePersonal} = require('../Services/useraccountservice')
+
+
+
 const bcrypt = require('bcrypt')
 
 const SchUserPersonalData = require('../Models/Schemas/schuserpersonaldata')
@@ -18,38 +23,51 @@ const createUser = async (req, res) => {
     let regData = new UserRegisterData(new UserCreds(req.body.username, req.body.password),
                                        new UserPersonal(req.body.name, req.body.surname))
 
-    const hashedPassword = await bcrypt.hash(regData.userCreds.password, 10)
-    const schUserCreds = new SchUserCreds({ username: regData.userCreds.username, password: hashedPassword })
+    var letters = /^[A-Za-z]+$/;
 
-    schUserCreds.save()
-    .then(async() => {
-        const schUserPersonal = new SchUserPersonalData({
-            username: regData.userCreds.username,
-            name: regData.userPersonal.name,
-            surname:  regData.userPersonal.surname,
-            points: 0,
-            crntlvl: 0,
-            timeStamp: Date.now()
-        })
+    if(regData.userCreds.username.length==0||regData.userCreds.password.length==0||regData.userPersonal.name.length==0||regData.userPersonal.surname.length==0)
+    { 
+        res.status(400).send("Cant have empty fields") 
+    }
+    else if(!regData.userPersonal.name.match(letters)||!regData.userPersonal.surname.match(letters))
+    {
+        res.status(401).send("Name or Surname must have alphabet characters only")
 
-        schUserPersonal.save().catch(async (error) => {
+    } else if(regData.userCreds.password.length<6)
+    { 
+        res.status(402).send("Password is too short") 
+    }
+    else
+    {
+        await servTrySaveCreds(regData).then(async() => {
+            await  servTrySavePersonal(regData).then(async() => {   
+
+                res.status(200).send("User was created")
+
+            }).catch(async (error) => {
+
+                console.error(error)
+                await deletUserByUsername(regData.userCreds.username)
+                res.status(500).send("Error while creating user personals")
+
+            })
+    
+        }).catch(async (error) => {
             console.error(error)
-            await deletUserByUsername(regData.userCreds.username)
-            res.status(500).send("Error while creating user personals")
-          })
-        
-          res.status(200).send("User successfully created")
-
-    }).catch(async (error) => {
-        console.error(error)
-        res.status(404).send("Error while creating user creds")
-      })
-
+            res.status(404).send("Error while creating user creds")
+        })
+    }
 }
 
 const authUser = async (req, res) => {
 
     let creds = new UserCreds(req.body.username, req.body.password)
+
+    if(creds.password.lenth<6)
+    { 
+        res.status(401).send("Password is too short") 
+    }
+
     await SchUserCreds.findOne({ username: creds.username })
         .then(async (userA) => {
             if (await bcrypt.compare(creds.password, userA.password)) {
@@ -81,44 +99,59 @@ const updatePersonalData = async (req, res) => {
 
     let newPersonalData = new UserPersonal(req.body.name, req.body.surname)
 
-    await SchUserPersonalData.findOne({ username: req.body.username })
-        .then(async (userPersonal) => 
-        {
-            userPersonal.name =  newPersonalData.name
-            userPersonal.surname = newPersonalData.surname 
+    var letters = /^[A-Za-z]+$/;
+    if(!newPersonalData.name.match(letters)||!newPersonalData.surname.match(letters))
+    {
+        res.status(400).send("Name or Surname must have alphabet characters only")
+    }
+    else
+    {
+        await SchUserPersonalData.findOne({ username: req.body.username })
+            .then(async (userPersonal) => 
+            {
+                userPersonal.name =  newPersonalData.name
+                userPersonal.surname = newPersonalData.surname 
 
-            userPersonal.save()
-                .then(() => {
-                    res.status(200).send("Personal data was updated");
-                })
-                .catch(() => {
-                    return res.status(401).send(`Data wasn't updated : ${error}`)
-                })
-                    })  
-        .catch(() => {
-            return res.status(404).send('Cannot find user')
-        })
+                userPersonal.save()
+                    .then(() => {
+                        res.status(200).send("Personal data was updated");
+                    })
+                    .catch(() => {
+                        return res.status(401).send(`Data wasn't updated : ${error}`)
+                    })
+                        })  
+            .catch(() => {
+                return res.status(404).send('Cannot find user')
+            })
+    }    
 }
 
 const updatePassword = async (req, res) => {
 
-    await SchUserCreds.findOne({ username: req.body.username })
-        .then(async (user) => {
+    if(req.body.newPassword.length<6)
+    { 
+        res.status(401).send("Password is too short") 
+    }
+    else
+    {
+        await SchUserCreds.findOne({ username: req.body.username })
+            .then(async (user) => {
 
-                const hashedPassword = await bcrypt.hash(req.body.newPassword, 10)
-                user.password = hashedPassword
+                    const hashedPassword = await bcrypt.hash(req.body.newPassword, 10)
+                    user.password = hashedPassword
 
-                user.save()
-                .then(() => {
-                    res.status(200).send("Password was updated");
-                })
-                .catch((error) => {
-                    return res.status(400).send(`Password wasn't updated : ${error}`)
-                })
-        })
-        .catch(() => {
-            return res.status(400).send('Cannot find user')
-        })
+                    user.save()
+                    .then(() => {
+                        res.status(200).send("Password was updated");
+                    })
+                    .catch((error) => {
+                        return res.status(400).send(`Password wasn't updated : ${error}`)
+                    })
+            })
+            .catch(() => {
+                return res.status(400).send('Cannot find user')
+            })
+    }
 }
 
 const deleteAccount = async (req, res) => {
